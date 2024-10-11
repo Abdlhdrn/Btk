51c82d8b84462e9f9f22c9c5337bb3ee69dba614
hmkarabekir <hasipmertk@gmail.com>
2024-10-11 19:22:25 +0300

    Error

---
+++ Kupa_Boş_Kasedir/middlewares/token.js
@@ -0,0 +1,29 @@
+//npm install jsonwebtoken
+
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const secretKey = 'sizin-gizli-anahtarınız';
+
+// Middleware
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) return res.sendStatus(401);
+    
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+// Korunan bir rota
+app.get('/protected', authenticateToken, (req, res) => {
+    res.json({ message: 'Bu korunan bir alan.', user: req.user });
+});
+
+// Sunucuyu başlat
+app.listen(3000, () => {
+    console.log('Sunucu 9595 portunda çalışıyor...');
+});
